c67bfcdd4d65ac6d6aab6318ad32154b2942e714
Shivam <70581178+shivamkonkar@users.noreply.github.com>
2023-11-15 01:02:29 +0530

    Create Solution.java

---
+++ Easy Questions/Valid Palindrome/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() -1;
+        while(left < right){
+            if(!(s.charAt(left) >= 'a' && s.charAt(left) <= 'z' || s.charAt(left) >= '0' && s.charAt(left) <= '9')){
+                left++;
+                continue;
+            }
+            if(!(s.charAt(right) >= 'a' && s.charAt(right) <= 'z' || s.charAt(right) >= '0' && s.charAt(right) <= '9')){
+                right--;
+                continue;
+            }
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
